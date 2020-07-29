3471e89edff7065b4eed98588b8d02dc4b484463
Shivangi Gupta <shivangi.gupta9820@gmail.com>
2020-07-29 13:30:58 +0530

    optimized

--- LeetCode/Two_Sum.java
+++ LeetCode/Two_Sum.java
@@ -8,20 +8,14 @@ return [0, 1].
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        int ans[] = new int[2];
-        for(int i=0;i<n;i++)
-        {
-            for(int j = i+1;j<n;j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    ans[0]=i;
-                    ans[1]=j;
-                    break;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+            map.put(nums[i], i);
         }
-        return ans;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
