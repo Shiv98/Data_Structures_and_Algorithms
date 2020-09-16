8a10a7b3a5a69b37d8e005a5b67d482d59f4945b
Shivangi Gupta <shivangi.gupta9820@gmail.com>
2020-09-16 23:21:22 +0530

    dp day 2

--- Pepcoding/Level1/DynamicProgramming/climb_stairs.cpp
+++ Pepcoding/Level1/DynamicProgramming/climb_stairs.cpp
@@ -1,3 +1,13 @@
+/*
+1. You are given a number n, representing the number of stairs in a staircase.
+2. You are on the 0th step and are required to climb to the top.
+3. In one move, you are allowed to climb 1, 2 or 3 stairs.
+4. You are required to print the number of different paths via which you can climb to the top.
+Sample Input
+4
+Sample Output
+7
+*/
 #include<iostream>
 using namespace std;
 int countpath_memo(int n, int arr[])
