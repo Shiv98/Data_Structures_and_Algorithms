97cfd9aac06975839a178fc4410f7f5bf7eb4bb9
Shivangi Gupta <shivangi.gupta9820@gmail.com>
2020-10-03 22:56:57 +0530

    formatted

--- Pepcoding/Level1/DynamicProgramming/tiling_mx1.cpp
+++ Pepcoding/Level1/DynamicProgramming/tiling_mx1.cpp
@@ -1,4 +1,5 @@
-/* 1. You are given a number n and a number m separated by line-break representing the length and breadth of a n * m floor.
+/* 
+1. You are given a number n and a number m separated by line-break representing the length and breadth of a n * m floor.
 2. You've an infinite supply of m * 1 tiles.
 3. You are required to calculate and print the number of ways floor can be tiled using tiles.
 Sample Input
